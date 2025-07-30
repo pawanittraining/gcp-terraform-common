6798acf05f84a36597aa769df9905a1efc3cbe75
pawanittraining <98798781+pawanittraining@users.noreply.github.com>
2025-07-30 12:47:25 +0530

    updated startup script

--- custom-image/files/apache.sh
+++ custom-image/files/apache.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-  apt update
-  apt install -y apache2
-  systemctl enable apache2
-  systemctl start apache2
+
+# Update package list and install Apache & Git
+apt update
+apt install -y apache2 git
+
+# Enable and start Apache
+systemctl enable apache2
+systemctl start apache2
+
+# Clear the default web root (optional)
+rm -rf /var/www/html/*
+
+# Clone the latest code from GitHub
+git clone https://github.com/pawanittraining/sample-website.git /var/www/html/
+
+# Set correct permissions
+chown -R www-data:www-data /var/www/html
--- custom-image/image.tf
+++ custom-image/image.tf
@@ -1,54 +1,8 @@
 /*
  * This Terraform script creates a custom image from a Google Compute Engine instance.
  * It provisions a VM, stops it, and then creates an image from the stopped VM.
- * terraform destroy -target=google_compute_instance.demo_vm
- * terraform state rm google_compute_image.web_app_image
- * terraform apply -target=google_compute_instance.demo_vm -target=null_resource.stop_vm -target=google_compute_image.web_app_image
- * terraform apply -target=null_resource.stop_vm -target=google_compute_image.web_app_image
+
  */
-
-# resource "google_compute_instance" "demo_vm" {
-#   name         = "web-app-instance"
-#   machine_type = var.machine_type
-#   zone         = var.zone[0]
-
-#   tags = var.tags
-#   labels = merge(var.common_labels, { environment = var.environment }, {
-#     name = "demo-instance"
-#   })
-
-#   boot_disk {
-#     initialize_params {
-#       image = var.image
-#       labels = merge(var.common_labels, {
-#         environment = var.environment
-#       })
-#     }
-#   }
-#   network_interface {
-#     network = var.vpc_name
-
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
-
-#   metadata_startup_script = file("files/apache.sh")
-# }
-
-# resource "null_resource" "stop_vm" {
-#   provisioner "local-exec" {
-#     command = "gcloud compute instances stop ${google_compute_instance.demo_vm.name} --zone ${google_compute_instance.demo_vm.zone}"
-#   }
-# }
-
-# resource "google_compute_image" "web_app_image" {
-#   name        = "web-app-image-${var.environment}-${formatdate("YYYYMMDDhhmmss", timestamp())}"
-#   source_disk = google_compute_instance.demo_vm.boot_disk[0].source
-#   family      = "custom-debian"
-#   description = "Custom image created from demo VM for instance group template"
-# }
-
 
 resource "google_compute_instance" "demo_vm" {
   name         = "web-app-instance"
@@ -95,6 +49,9 @@ resource "google_compute_image" "web_app_image" {
   description = "Custom image created from demo VM"
 
   depends_on = [null_resource.stop_vm]
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # (Optional) Step 4: Delete the instance after image creation
